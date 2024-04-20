75175fb662b108c9bc1bf9918cab2c1a2db73eaf
Geo-99 <georgstarz@web.de>
2024-04-20 19:45:24 +0200

    including Spain example data

---
+++ R/spain_gdp.R
@@ -0,0 +1,14 @@
+#' GDP data of Spanish federal states
+#'
+#' This dataset contains the GDP per capita and the total GDP for all Spanish federal states in 2022.
+#'
+#' @format ##'spain_gdp'
+#' A data frame with 19 rows and 4 columns:
+#' \describe{
+#'  \item{CCAA}{Name of the state}
+#'  \item{Fecha}{Year of the data}
+#'  \item{PIB_Per_Capita_EURO}{GDP per capita in euros}
+#'  \item{PIB_anual_EURO}{Total GDP of the state in euros}
+#'  }
+#'  @source <https://datosmacro.expansion.com/pib/espana-comunidades-autonomas>
+"spain_gdp"
---
+++ R/spain_gpkg.R
@@ -0,0 +1,14 @@
+#' Geodata of Spanish federal states
+#'
+#' This dataset contains geodata of all Spanish federal states.
+#'
+#' @format ##'spain_gpkg'
+#' A data frame with 19 rows and 4 columns:
+#' \describe{
+#'  \item{Codigo}{Numeric code of the state}
+#'  \item{Texto}{Name of the state}
+#'  \item{Texto_Alt}{Name of the state in the regional language}
+#'  \item{geom}{Geometry column}
+#'  }
+#'  @source <https://www.arcgis.com/home/item.html?id=5f689357238847bc823a2fb164544a77#!>
+"spain_gpkg"
